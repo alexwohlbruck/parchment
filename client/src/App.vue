416e72956b8c5884adab047a70bc51190555450c
Alex Wohlbruck <alexwohlbruck@gmail.com>
2023-11-20 01:09:26 -0500

    Add PrimeVue, navigation menu

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,85 +1,33 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+import { ref, watch } from 'vue'
+import NavigationMenu from './components/organisms/NavigationMenu.vue'
+import { useMagicKeys } from '@vueuse/core'
+
+const visible = ref(false)
+const keys = useMagicKeys()
+const CmdK = keys['/']
+
+watch(CmdK, (v) => {
+  if (v) {
+    console.log('Meta + K has been pressed')
+    visible.value = true
+  }
+})
+
+import CommandPalette from './components/organisms/CommandPalette.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <NavigationMenu />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <CommandPalette
+    :visible="visible"
+    @update:visible="
+      (newValue) => {
+        console.log(newValue)
+        visible = newValue
+      }
+    "
+  />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>

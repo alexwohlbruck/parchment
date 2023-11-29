78c53fcc4364ce7b4588b4c0c0961ed64b320734
Alex Wohlbruck <c-alex.wohlbruck@charter.com>
2023-11-29 18:01:39 -0500

    Experimenting with CyclOSM tiles

--- client/src/components/organisms/Map.vue
+++ client/src/components/organisms/Map.vue
@@ -50,164 +50,170 @@ onMounted(() => {
     attributionControl: false
   })
 
-  // Transit map
-  map.on('style.load', () => {
-    // Add Transitland source
-    map.addSource('routes', {
-      type: 'vector',
-      tiles: [
-        `https://transit.land/api/v2/tiles/routes/tiles/{z}/{x}/{y}.pbf?apikey=${transitlandApiKey}`
-      ],
-      maxzoom: 14
-    })
-
-    map.addLayer({
-      id: 'routes',
-      type: 'line',
-      source: 'routes',
-      'source-layer': 'routes',
-      layout: {
-        'line-cap': 'round',
-        'line-join': 'round',
-        'symbol-placement': 'line',
-        'text-field': ['get', 'route_long_name'], // use the route_name property for the text
-        'text-size': 12,
-        'text-offset': [0, 1] // adjust the text offset if needed,
-      },
-      paint: {
-        'line-width': 3.0,
-        'line-color': [
-          'match',
-          ['get', 'route_type'],
-          0, // Tram, streetcar, light rail
-          ['coalesce', ['get', 'route_color'], 'blue'],
-          1, // Subway, metro
-          ['coalesce', ['get', 'route_color'], 'blue'],
-          2, // Rail
-          'red', // amtrak
-          3, // Bus
-          ['coalesce', ['get', 'route_color'], 'lightblue'], // busses
-          4, // Ferry
-          'blue',
-          5, // Cable tram
-          'purple',
-          6, // Aerial lift, suspended cable car
-          'pink',
-          7, // Funicular
-          'brown',
-          11, // Trolleybus
-          'black',
-          12, // Monorail
-          'red',
-          'grey' // default
-        ],
-        'line-opacity': [
-          'match',
-          ['get', 'route_type'],
-          0, // Tram, streetcar, light rail
-          1,
-          1, // Subway, metro
-          1,
-          2, // Rail
-          1,
-          3, // Bus
-          1,
-          4, // Ferry
-          0,
-          5, // Cable tram
-          1,
-          6, // Aerial lift, suspended cable car
-          1,
-          7, // Funicular
-          1,
-          11, // Trolleybus
-          1,
-          12, // Monorail
-          1,
-          0
-        ]
-        // 'line-blur': 1
-        // 'line-offset': 1
-      }
-    })
-
-    // Show stops from transitland
-    map.addSource('stops', {
-      type: 'vector',
-      tiles: [
-        'mapbox://styles/mapbox/streets-v12',
-        `https://transit.land/api/v2/tiles/stops/tiles/{z}/{x}/{y}.pbf?apikey=${transitlandApiKey}`
-      ],
-      maxzoom: 14
-    })
-
-    map.addLayer({
-      id: 'stops',
-      type: 'circle',
-      source: 'stops',
-      'source-layer': 'stops',
-      layout: {},
-      paint: {
-        'circle-radius': 4,
-        'circle-color': 'grey',
-        'circle-stroke-color': 'white',
-        'circle-stroke-width': 1
-      }
-    })
-
-    const layers = map.getStyle().layers
-    const textLayerId = layers.find(
-      (layer) => layer.type === 'symbol' && layer.layout['text-field']
-    ).id
-
-    if (textLayerId) {
-      map.moveLayer('routes', textLayerId)
-      map.moveLayer('stops', textLayerId)
-    }
-  })
-
   map.addControl(new NavigationControl(), 'top-right')
   map.addControl(new GeolocateControl(), 'top-right')
   map.addControl(new AttributionControl(), 'bottom-right')
   map.addControl(new ScaleControl(), 'bottom-right')
 
-  // POI hover
-  map.on('mousemove', (e) => {
-    const features = map.queryRenderedFeatures(e.point, {
-      layers: ['poi-label']
+  // Transit map
+  map.on('style.load', () => {
+    console.log(map)
+
+    map.addSource('cycleosm', {
+      type: 'raster',
+      tiles: ['https://a.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png'],
+      tileSize: 256,
+      attribution: '<a href="https://www.opencyclemap.org/">© OpenCycleMap</a>'
     })
 
-    if (features.length) {
-      map.getCanvas().style.cursor = 'pointer'
-    } else {
-      map.getCanvas().style.cursor = ''
-    }
-  })
+    map.addLayer({
+      id: 'cycleosm-layer',
+      type: 'raster',
+      source: 'cycleosm',
+      minzoom: 0,
+      maxzoom: 22
+    })
 
-  // POI click popup
-  map.on('click', (e) => {
-    const features = map.queryRenderedFeatures(e.point)
-
-    if (!features.length) {
-      return
-    }
-
-    const feature = features[0]
-
-    console.log(feature)
-
-    // drop marker pin
-    const marker = new Marker().setLngLat(feature.geometry.coordinates).addTo(map)
-    router.push('/map/place')
-
-    // const popup = new Popup({
-    //   closeButton: false,
-    //   closeOnClick: false
+    // Add Transitland source
+    // map.addSource('routes', {
+    //   type: 'vector',
+    //   tiles: [
+    //     `https://transit.land/api/v2/tiles/routes/tiles/{z}/{x}/{y}.pbf?apikey=${transitlandApiKey}`
+    //   ],
+    //   maxzoom: 14
     // })
-    //   .setLngLat(feature.geometry.coordinates)
-    //   .setHTML(feature.properties.name)
-    //   .addTo(map)
+
+    // map.addLayer({
+    //   id: 'routes',
+    //   type: 'line',
+    //   source: 'routes',
+    //   'source-layer': 'routes',
+    //   layout: {
+    //     'line-cap': 'round',
+    //     'line-join': 'round',
+    //     'symbol-placement': 'line',
+    //     'text-field': ['get', 'route_long_name'], // use the route_name property for the text
+    //     'text-size': 12,
+    //     'text-offset': [0, 1] // adjust the text offset if needed,
+    //   },
+    //   paint: {
+    //     'line-width': 2.0,
+    //     'line-color': [
+    //       'match',
+    //       ['get', 'route_type'],
+    //       0, // Tram, streetcar, light rail
+    //       ['coalesce', ['get', 'route_color'], 'blue'],
+    //       1, // Subway, metro
+    //       ['coalesce', ['get', 'route_color'], 'blue'],
+    //       2, // Rail
+    //       'red', // amtrak
+    //       3, // Bus
+    //       ['coalesce', ['get', 'route_color'], 'lightblue'], // busses
+    //       4, // Ferry
+    //       'blue',
+    //       5, // Cable tram
+    //       'purple',
+    //       6, // Aerial lift, suspended cable car
+    //       'pink',
+    //       7, // Funicular
+    //       'brown',
+    //       11, // Trolleybus
+    //       'black',
+    //       12, // Monorail
+    //       'red',
+    //       'grey' // default
+    //     ],
+    //     'line-opacity': [
+    //       'match',
+    //       ['get', 'route_type'],
+    //       0, // Tram, streetcar, light rail
+    //       1,
+    //       1, // Subway, metro
+    //       1,
+    //       2, // Rail
+    //       1,
+    //       3, // Bus
+    //       0,
+    //       4, // Ferry
+    //       1,
+    //       5, // Cable tram
+    //       1,
+    //       6, // Aerial lift, suspended cable car
+    //       1,
+    //       7, // Funicular
+    //       1,
+    //       11, // Trolleybus
+    //       1,
+    //       12, // Monorail
+    //       1,
+    //       0
+    //     ]
+    //     // 'line-blur': 1
+    //     // 'line-offset': 1
+    //   }
+    // })
+
+    // // Show stops from transitland
+    // map.addSource('stops', {
+    //   type: 'vector',
+    //   tiles: [
+    //     'mapbox://styles/mapbox/streets-v12',
+    //     `https://transit.land/api/v2/tiles/stops/tiles/{z}/{x}/{y}.pbf?apikey=${transitlandApiKey}`
+    //   ],
+    //   maxzoom: 14
+    // })
+
+    // map.addLayer({
+    //   id: 'stops',
+    //   type: 'circle',
+    //   source: 'stops',
+    //   'source-layer': 'stops',
+    //   layout: {},
+    //   paint: {
+    //     'circle-radius': 3,
+    //     'circle-color': 'grey',
+    //     'circle-stroke-color': 'white',
+    //     'circle-stroke-width': 1
+    //   }
+    // })
+
+    // const layers = map.getStyle().layers
+    // const textLayerId = layers.find(
+    //   (layer) => layer.type === 'symbol' && layer.layout['text-field']
+    // ).id
+
+    // if (textLayerId) {
+    //   map.moveLayer('routes', textLayerId)
+    //   map.moveLayer('stops', textLayerId)
+    // }
   })
+
+  // // POI hover
+  // map.on('mousemove', (e) => {
+  //   const features = map.queryRenderedFeatures(e.point, {
+  //     layers: ['poi-label']
+  //   })
+
+  //   if (features.length) {
+  //     map.getCanvas().style.cursor = 'pointer'
+  //   } else {
+  //     map.getCanvas().style.cursor = ''
+  //   }
+  // })
+
+  // // POI click popup
+  // map.on('click', (e) => {
+  //   const features = map.queryRenderedFeatures(e.point)
+
+  //   if (!features.length) {
+  //     return
+  //   }
+
+  //   const feature = features[0]
+
+  //   const marker = new Marker().setLngLat(feature.geometry.coordinates).addTo(map)
+  //   router.push('/map/place')
+  // })
 })
 
 onUnmounted(() => {

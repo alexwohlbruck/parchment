469ef52204b02b13adc868f1bd86f48592ae818d
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-01-04 18:21:54 -0500

    Add mapbox embed

--- client/src/views/Map.vue
+++ client/src/views/Map.vue
@@ -1,1 +1,10 @@
-<template>Map</template>
+<script setup lang="ts">
+import Map from "../components/Map.vue";
+</script>
+
+<template>
+  <div class="column w-full h-full p-0">
+    <router-view class="z-2" />
+    <Map class="!absolute w-full h-full top-0 left-0"></Map>
+  </div>
+</template>

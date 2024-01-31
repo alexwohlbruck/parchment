1645618edb68e165e5481600d80f0dcf90224829
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-01-30 23:49:40 -0500

    Elysia testing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,24 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(5000);
+const app = new Elysia()
+
+app.group('/layers', (app) =>
+  app.get('/', ({ set }) => {
+    return {
+      layers: [
+        { id: 1, name: 'Layer 1' },
+        { id: 2, name: 'Layer 2' },
+      ],
+    }
+  }),
+)
+
+app.onError(({ code }) => {
+  if (code === 'NOT_FOUND') return 'Route not found :('
+})
+
+app.listen(5000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+)

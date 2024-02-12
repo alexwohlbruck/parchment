9dcdd508476eefc66008eb90c11a6c81ebff51c7
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-02-11 23:54:31 -0500

    Begin strategy pattern for map SDK selection

--- client/src/components/map/Map.vue
+++ client/src/components/map/Map.vue
@@ -3,131 +3,51 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted, watch, computed } from "vue";
+import { ref, onMounted, onUnmounted, watch, computed } from 'vue'
 // import { useRouter } from "vue-router";
-import {
-  Map,
-  NavigationControl,
-  GeolocateControl,
-  AttributionControl,
-  ScaleControl,
-  // Marker,
-  // Popup,
-  // Layer,
-  // Source,
-  // LngLatBounds,
-  // LngLat,
-} from "mapbox-gl";
-import { layers } from "./layers";
 // import { useSettingsStore } from "@/stores/settings";
 // import { AppTheme } from "../../types/settings";
-import { useDark } from "@vueuse/core";
-import { useMapStore } from "../../stores/map.store";
+import { useDark } from '@vueuse/core'
+import { useMapStore } from '../../stores/map.store'
+import { MapboxStrategy } from './map-providers/mapbox.strategy'
+import { MapStrategy } from './map-providers/map.strategy'
 
-const projection: any = localStorage.getItem("projection") || "globe";
+const dark = useDark()
+const mapStore = useMapStore()
 
-// const router = useRouter();
-// const settingsStore = useSettingsStore();
+const mapContainer = ref(null)
+let map: MapStrategy
 
-const dark = useDark();
-const mapStore = useMapStore();
-
-const mapContainer = ref(null);
-let map;
+function getMapInstance() {
+  const mapSDK: string = 'mapbox'
+  const options = {
+    dark: dark.value,
+  }
+  switch (mapSDK) {
+    case 'mapbox':
+      return new MapboxStrategy(mapContainer.value, options)
+    default: {
+      throw new Error('Map SDK not selected')
+    }
+  }
+}
 
 onMounted(() => {
-  // For testing
-  const { lng, lat, zoom, bearing, pitch } = {
-    lng: -80.8432808,
-    lat: 35.2205601,
-    bearing: 0,
-    pitch: 0,
-    zoom: 14,
-  };
+  map = getMapInstance()
 
-  map = new Map({
-    accessToken: import.meta.env.VITE_MAPBOX_ACCESS_TOKEN,
-    container: mapContainer.value as any,
-    style: "mapbox://styles/mapbox/standard-beta",
-    center: [lng, lat],
-    bearing,
-    pitch,
-    zoom,
-    attributionControl: false,
-    projection: {
-      name: projection,
-    },
-  });
-
-  map.addControl(new ScaleControl(), "bottom-right");
-  map.addControl(new NavigationControl(), "bottom-right");
-  map.addControl(new GeolocateControl(), "bottom-right");
-  map.addControl(
-    new AttributionControl({
-      compact: true,
-    }),
-    "bottom-left"
-  );
-
-  map.on("load", mapInit);
-  map.on("style.load", mapInit);
-
-  function mapInit() {
-    addLayers();
-    setMapTheme(dark.value);
-    // togglePoiLabels(false);
-  }
-
-  function addLayers() {
-    Object.values(layers).forEach((layerType) => {
-      layerType.layers.forEach((layer) => {
-        if (!layer.enabled) return;
-        const { meta, source } = layer;
-        const id = source.id;
-        map.addSource(id, {
-          id,
-          ...source,
-        });
-        map.addLayer({
-          source: id,
-          id,
-          ...meta,
-          slot: "middle",
-        });
-      });
-    });
-  }
-
-  function setMapTheme(dark: boolean) {
-    let lightPreset;
-    switch (dark) {
-      case false:
-        lightPreset = "day";
-        break;
-      case true:
-        lightPreset = "night";
-        break;
-    }
-    map.setConfigProperty("basemap", "lightPreset", lightPreset);
-  }
-
-  function togglePoiLabels(value: boolean) {
-    map.setConfigProperty("basemap", "showPointOfInterestLabels", value);
-  }
-
-  watch(dark, setMapTheme);
+  watch(dark, map.setMapTheme)
   watch(
     () => mapStore.activeBasemapName,
-    (name) => {
-      console.log(mapStore.basemaps, name);
-      map.setStyle(mapStore.basemaps[name].url);
+    name => {
+      console.log(mapStore.basemaps, name)
+      map.setStyle(mapStore.basemaps[name].url)
     }
-  );
-});
+  )
+})
 
 onUnmounted(() => {
-  map.remove();
-});
+  map.remove()
+})
 </script>
 
 <style>

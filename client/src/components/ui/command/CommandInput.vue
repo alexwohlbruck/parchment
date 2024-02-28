4d03cd24c0b37b1217debe544060c97bea16111e
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-02-27 21:30:22 -0500

    Nuke command components

--- client/src/components/ui/command/CommandInput.vue
+++ client/src/components/ui/command/CommandInput.vue
@@ -1,64 +1,33 @@
 <script setup lang="ts">
-import { type HTMLAttributes, computed, ref } from 'vue'
+import { type HTMLAttributes, computed } from 'vue'
 import { Search } from 'lucide-vue-next'
-import {
-  ComboboxInput,
-  type ComboboxInputProps,
-  useForwardProps,
-} from 'radix-vue'
+import { ComboboxInput, type ComboboxInputProps, useForwardProps } from 'radix-vue'
 import { cn } from '@/lib/utils'
 
 defineOptions({
   inheritAttrs: false,
 })
 
-const props = defineProps<
-  ComboboxInputProps & {
-    class?: HTMLAttributes['class']
-    modelValue: string
-  }
->()
-
-const emits = defineEmits<{
-  'update:modelValue': any
-  focus: any
-  blur: any
+const props = defineProps<ComboboxInputProps & {
+  class?: HTMLAttributes['class']
 }>()
 
 const delegatedProps = computed(() => {
   const { class: _, ...delegated } = props
+
   return delegated
 })
 
 const forwardedProps = useForwardProps(delegatedProps)
-
-const input = ref<InstanceType<typeof ComboboxInput> | null>(null)
-const inputElement = computed(() => input.value?.$el as HTMLInputElement | null)
-
-defineExpose({ inputElement })
 </script>
 
 <template>
-  <div
-    class="flex items-center border-b dark:border-neutral-900 px-3"
-    cmdk-input-wrapper
-  >
+  <div class="flex items-center border-b px-3" cmdk-input-wrapper>
     <Search class="mr-2 h-4 w-4 shrink-0 opacity-50" />
     <ComboboxInput
-      ref="input"
-      :autofocus="false"
       v-bind="{ ...forwardedProps, ...$attrs }"
-      :value="props.modelValue"
-      @input="$event => $emit('update:modelValue', $event.target.value)"
-      @focus="$event => $emit('focus', $event)"
-      @blur="$event => $emit('blur', $event)"
-      :class="
-        cn(
-          'flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
-          props.class,
-        )
-      "
+      auto-focus
+      :class="cn('flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50', props.class)"
     />
-    <slot />
   </div>
 </template>

80860ac3906091451d61fce6bf9fa131142d5f05
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-02-28 17:46:35 -0500

    Implement hotkeys help menu

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import Nav from '@/components/navigation/Navigation.vue'
-import Palette from '@/components/palette/Palette.vue'
 import { onMounted } from 'vue'
 import { useConfigStore } from './stores/settings.store'
 import { useCommandService } from './services/command/command.service'
+
+import Nav from '@/components/navigation/Navigation.vue'
+import Palette from '@/components/palette/Palette.vue'
+import HotkeysMenu from '@/components/HotkeysMenu.vue'
 
 const { theme, radius } = useConfigStore()
 const commandService = useCommandService()
@@ -22,12 +24,19 @@ onMounted(() => {
       <Nav class="z-20" />
     </div>
 
+    <div
+      class="absolute top-1/2 left-1/2 transform -translate-x-1/2 p-2 z-10 w-1/2 max-w-[30rem]"
+    >
+      <HotkeysMenu />
+    </div>
+
+    <div
+      class="absolute top-0 left-1/2 transform -translate-x-1/2 p-2 z-10 w-1/2 max-w-[30rem]"
+    >
+      <Palette class="h-fit" />
+    </div>
+
     <main class="flex-1">
-      <div
-        class="absolute top-0 left-1/2 transform -translate-x-1/2 p-2 z-10 w-1/2 max-w-[30rem]"
-      >
-        <Palette class="h-fit" />
-      </div>
       <RouterView />
     </main>
   </div>

db0ba0cae4dc64a3537748056a3096123335ab86
Alex Wohlbruck <c-alex.wohlbruck@charter.com>
2024-03-06 20:00:53 -0500

    Add navigation command

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,52 +1,52 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Map from "../views/Map.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Map from '../views/Map.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "map",
+    path: '/',
+    name: 'Map',
     component: Map,
     children: [
       {
-        path: "/place",
-        name: "place",
-        component: () => import("../views/Place.vue"),
+        path: '/place',
+        name: 'Place',
+        component: () => import('../views/Place.vue'),
       },
     ],
   },
   {
-    path: "/settings",
-    name: "settings",
-    component: () => import("../views/settings/Settings.vue"),
-    redirect: "/settings/appearance",
+    path: '/settings',
+    name: 'Settings',
+    component: () => import('../views/settings/Settings.vue'),
+    redirect: '/settings/appearance',
     children: [
       {
-        path: "/settings/account",
-        name: "account",
-        component: () => import("../views/settings/Account.vue"),
+        path: '/settings/account',
+        name: 'Account',
+        component: () => import('../views/settings/Account.vue'),
       },
       {
-        path: "/settings/behavior",
-        name: "behavior",
-        component: () => import("../views/settings/Behavior.vue"),
+        path: '/settings/behavior',
+        name: 'Behavior',
+        component: () => import('../views/settings/Behavior.vue'),
       },
       {
-        path: "/settings/appearance",
-        name: "appearance",
-        component: () => import("../views/settings/appearance/Appearance.vue"),
+        path: '/settings/appearance',
+        name: 'Appearance',
+        component: () => import('../views/settings/appearance/Appearance.vue'),
       },
       {
-        path: "/settings/map-data",
-        name: "map-data",
-        component: () => import("../views/settings/MapData.vue"),
+        path: '/settings/map-data',
+        name: 'Map data',
+        component: () => import('../views/settings/MapData.vue'),
       },
     ],
   },
-];
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
-});
+})
 
-export default router;
+export default router

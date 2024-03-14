9a70d7a94ecb93e5e75ae2f6f85da2a5936e5f3b
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-03-14 17:30:53 -0400

    Add change language command

--- client/src/lib/i18n/index.ts
+++ client/src/lib/i18n/index.ts
@@ -1,14 +1,16 @@
 import { createI18n } from 'vue-i18n'
 import enUS from './en-US.json'
+import esES from './es-ES.json'
 
 type TranslationsSchema = typeof enUS
-type Locale = 'en-US' // | 'es-ES'
+export type Locale = 'en-US' | 'es-ES'
 
 export const i18n = createI18n<[TranslationsSchema], Locale>({
   legacy: false,
-  // locale: 'en-US',
+  locale: 'es-ES',
   // fallbackLocale: 'en-US',
   messages: {
     'en-US': enUS,
+    'es-ES': esES,
   },
 })

9549a3c3427afcf4b48184444cf08dea71677302
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-03-27 23:30:17 -0400

    Begin auth with Lucia, Drizzle

--- server/src/schema/user.ts
+++ server/src/schema/user.ts
@@ -1,7 +1,8 @@
 import { pgTable, serial, text } from 'drizzle-orm/pg-core'
 
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
+export const users = pgTable('user', {
+  id: text('id').primaryKey(),
+  email: text('email').unique().notNull(),
   first_name: text('first_name'),
   last_name: text('last_name'),
   picture: text('picture'),

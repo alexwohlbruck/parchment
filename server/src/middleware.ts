3eafb7b19960622825ff54b79e2ddb3ab34b99a3
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-04-07 00:21:32 -0400

    Add auth middleware

---
+++ server/src/middleware.ts
@@ -0,0 +1,57 @@
+import { Elysia, CookieOptions } from 'elysia'
+import { verifyRequestOrigin } from 'lucia'
+import type { User, Session } from 'lucia'
+
+import { lucia } from './lucia'
+
+export const auth = (app: Elysia) =>
+  app.derive(
+    async ({
+      request,
+      cookie,
+    }): Promise<{
+      user: User | null
+      session: Session | null
+    }> => {
+      // CSRF check
+      if (request.method !== 'GET') {
+        const originHeader = request.headers.get('Origin')
+        const hostHeader = request.headers.get('Host')
+
+        if (
+          !originHeader ||
+          !hostHeader ||
+          !verifyRequestOrigin(originHeader, [hostHeader])
+        ) {
+          return {
+            user: null,
+            session: null,
+          }
+        }
+      }
+
+      // const sessionId = lucia_session.value
+      const cookieHeader = request.headers.get('Cookie') ?? ''
+      const sessionId = lucia.readSessionCookie(cookieHeader)
+      if (!sessionId) {
+        return {
+          user: null,
+          session: null,
+        }
+      }
+
+      const { session, user } = await lucia.validateSession(sessionId)
+      if (session && session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(session.id)
+
+        cookie[sessionCookie.name].set({
+          value: sessionCookie.value,
+          ...sessionCookie.attributes,
+        })
+      }
+      return {
+        user,
+        session,
+      }
+    },
+  )

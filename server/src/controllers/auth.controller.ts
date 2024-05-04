22f19328fc0c7ccd84f376bda286979243b04b4d
Alex Wohlbruck <c-alex.wohlbruck@charter.com>
2024-05-04 15:40:29 -0400

    Move business logic to controllers

---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,330 @@
+import Elysia, { t } from 'elysia'
+import { eq } from 'drizzle-orm'
+import { db } from '../db'
+import { users } from '../schema/user'
+import { auth } from '../middleware/auth.middleware'
+import { generateId } from '../util'
+import { origins } from '../config'
+import { Passkey, passkeys } from '../schema/passkey'
+import {
+  createSession,
+  destroySession,
+  generateWebauthnOptions,
+  rpID,
+  sendEmailVerificationCode,
+} from '../services/auth.service'
+import { fetchUser, fetchUserByEmail } from '../services/user.service'
+import {
+  createServerToken,
+  validateServerToken,
+} from '../services/token.service'
+import {
+  verifyAuthenticationResponse,
+  verifyRegistrationResponse,
+} from '@simplewebauthn/server'
+import {
+  AuthenticationResponseJSON,
+  AuthenticatorTransportFuture,
+  RegistrationResponseJSON,
+} from '@simplewebauthn/server/script/deps'
+
+const app = new Elysia({ prefix: '/auth' })
+
+/**
+ * Temporary function to create an initial user in the DB.
+ * Once sign in route can automatically create users, this can be removed
+ */
+app.post('me', async () => {
+  return db
+    .insert(users)
+    .values({
+      id: '1',
+      firstName: 'Alex',
+      lastName: 'Wohlbruck',
+      email: 'alexwohlbruck@gmail.com',
+      picture: 'https://github.com/alexwohlbruck.png',
+    })
+    .returning()
+})
+
+app.post(
+  'verify',
+  async ({ body: { email }, set }) => {
+    let user = await fetchUserByEmail(email)
+
+    if (!user) {
+      const userId = generateId()
+
+      user = (
+        await db
+          .insert(users)
+          .values({
+            id: userId,
+            email,
+          })
+          .returning()
+      )[0]
+    }
+
+    const verificationCode = await createServerToken('otp', user.id)
+    const emailSuccess = await sendEmailVerificationCode(
+      user.email,
+      verificationCode,
+    )
+
+    if (emailSuccess) {
+      set.status = 201
+    } else {
+      set.status = 500
+    }
+  },
+  {
+    detail: {
+      tags: ['Auth'],
+      description: 'Verify an email address by requesting a one-time password.',
+    },
+    body: t.Object({
+      email: t.String({
+        format: 'email',
+      }),
+    }),
+  },
+)
+
+app.group('/webauthn', (app) =>
+  app
+    .group('/register', (app) =>
+      app
+        .use(auth)
+        .post('/options', async ({ user, cookie: { challenge }, set }) => {
+          if (!user) {
+            set.status = 401
+            return user
+          }
+
+          const { email } = await fetchUser(user.id)
+
+          try {
+            const options = await generateWebauthnOptions(
+              'register',
+              user.id,
+              email,
+            )
+            challenge.value = options.challenge
+            return options
+          } catch (err) {
+            set.status = 500
+          }
+        })
+
+        // Create new passkey
+        .post(
+          '/verify',
+          async ({ body, set, user, cookie: { challenge } }) => {
+            if (!user) return (set.status = 401)
+            if (!challenge.value) return (set.status = 400) // TODO: Check this is how to break out with error in Elysia, make better error
+
+            const verification = await verifyRegistrationResponse({
+              response: body as RegistrationResponseJSON,
+              expectedChallenge: challenge.value,
+              expectedOrigin: origins.clientOrigin,
+              expectedRPID: rpID,
+              requireUserVerification: true,
+            })
+
+            if (!verification.verified) {
+              set.status = 401
+              return null
+            }
+
+            const { registrationInfo } = verification
+
+            if (!registrationInfo) {
+              // TODO: Return correct error code and message
+              set.status = 401
+              return null
+            }
+
+            const {
+              credentialID,
+              credentialPublicKey,
+              counter,
+              credentialDeviceType,
+              credentialBackedUp,
+            } = registrationInfo
+
+            console.log(verification)
+
+            const passkey: Partial<Passkey> = (
+              await db
+                .insert(passkeys)
+                .values({
+                  id: credentialID,
+                  publicKey:
+                    Buffer.from(credentialPublicKey).toString('base64'),
+                  userId: user.id,
+                  counter,
+                  deviceType: credentialDeviceType,
+                  backedUp: credentialBackedUp,
+                  transports: body.response.transports,
+                })
+                .returning()
+            )[0]
+
+            // TODO: Find way to automatically hide sensitive fields
+            delete passkey.publicKey
+            challenge.remove()
+
+            return passkey
+          },
+          {
+            detail: {
+              tags: ['Auth'],
+              description: 'Verify webauthn passkey registration.',
+            },
+            body: t.Object({
+              id: t.String(),
+              rawId: t.String(),
+              response: t.Object({
+                clientDataJSON: t.String(),
+                attestationObject: t.String(),
+                authenticatorData: t.Optional(t.String()),
+                transports: t.Optional(t.Any()),
+                publicKeyAlgorithm: t.Optional(t.Any()),
+                publicKey: t.Optional(t.String()),
+              }),
+              authenticatorAttachment: t.Any(),
+              clientExtensionResults: t.Optional(t.Any()),
+              type: t.String(),
+            }),
+          },
+        ),
+    )
+
+    .group('/authenticate', (app) =>
+      app
+        .post('options', async ({ set, cookie: { challenge } }) => {
+          try {
+            const options = await generateWebauthnOptions('authenticate')
+            challenge.value = options.challenge
+            return options
+          } catch (err) {
+            set.status = 500
+          }
+        })
+        .post(
+          'verify',
+          async ({ body, set, cookie: { challenge } }) => {
+            if (!challenge.value) return (set.status = 400) // TODO: Make better error
+
+            const passkey = (
+              await db.select().from(passkeys).where(eq(passkeys.id, body.id))
+            )[0]
+
+            if (!passkey) return (set.status = 401) // TODO: Make better error
+
+            const verification = await verifyAuthenticationResponse({
+              response: body as AuthenticationResponseJSON,
+              expectedChallenge: challenge.value,
+              expectedOrigin: origins.clientOrigin,
+              expectedRPID: rpID,
+              authenticator: {
+                credentialID: passkey.id,
+                credentialPublicKey: Buffer.from(passkey.publicKey, 'base64'),
+                counter: passkey.counter,
+                transports: passkey.transports.split(
+                  ',',
+                ) as AuthenticatorTransportFuture[],
+              },
+            })
+
+            if (verification.verified) {
+              const user = await fetchUser(passkey.userId)
+              const session = await createSession(user.id, set)
+              set.status = 201
+              return {
+                token: session.id,
+                user,
+              }
+            }
+
+            challenge.remove()
+            set.status = 401
+          },
+          {
+            body: t.Object({
+              id: t.String(),
+              rawId: t.String(),
+              response: t.Object({
+                authenticatorData: t.String(),
+                clientDataJSON: t.String(),
+                signature: t.String(),
+                userHandle: t.String(),
+              }),
+              type: t.String(),
+              clientExtensionResults: t.Any(),
+              authenticatorAttachment: t.String(),
+            }),
+          },
+        ),
+    ),
+)
+
+app.group('/sessions', (app) =>
+  app
+    .post(
+      '/',
+      async ({ body: { email, token }, set, error }) => {
+        const user = await fetchUserByEmail(email)
+
+        if (!user) {
+          return error(404, 'User not found')
+        }
+
+        const { id: userId } = await fetchUserByEmail(email)
+        const isValid = await validateServerToken(token, 'otp', userId)
+
+        if (!isValid) return error(401)
+
+        const session = await createSession(user.id, set)
+
+        return {
+          token: session.id,
+          user,
+        }
+      },
+      {
+        detail: {
+          tags: ['Auth'],
+          description:
+            'Sign in a user. Exchanges a passkey or one-time password for an authentication token.',
+        },
+        body: t.Union([
+          t.Object({
+            method: t.Union([t.Literal('passkey'), t.Literal('otp')]),
+            email: t.String({
+              format: 'email',
+            }),
+            // TODO: Accept 8 digit otp or server token
+            token: t.String(),
+          }),
+        ]),
+      },
+    )
+
+    .delete(
+      '/',
+      async ({ cookie, set }) => {
+        destroySession(cookie)
+        set.status = 204
+      },
+      {
+        detail: {
+          tags: ['Auth'],
+          description: 'Sign out a user.',
+        },
+      },
+    ),
+)
+
+export default app
---
+++ server/src/controllers/index.ts
@@ -0,0 +1,4 @@
+export const appName = 'Parchment Maps'
+
+export { default as auth } from './auth.controller'
+export { default as user } from './user.controller'

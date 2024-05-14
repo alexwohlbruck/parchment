50105188cbcd3a2fff01d1d8e84daecc56148d3b
Alex Wohlbruck <c-alex.wohlbruck@charter.com>
2024-05-13 21:12:47 -0400

    Begin standalone sign in page

---
+++ client/types.d.ts
@@ -0,0 +1,12 @@
+import 'vue-router'
+
+export {}
+
+// TODO: This isn't working
+declare module 'vue-router' {
+  interface RouteMeta {
+    auth?: boolean
+    isAdmin?: boolean
+    layout?: 'floating' | 'sidebar'
+  }
+}

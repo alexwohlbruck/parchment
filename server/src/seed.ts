5ee4ffb1f8d1746ff148696158a142f3d842a6b4
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-05-29 00:37:05 -0400

    Add permission middleware

--- server/src/seed.ts
+++ server/src/seed.ts
@@ -4,32 +4,47 @@ import { roles } from './schema/role.schema'
 import { roleToPermissions } from './schema/role-permission.schema'
 import { usersToRoles } from './schema/user-role.schema'
 
-await db.insert(roles).values({
-  id: 'user',
-  name: 'User',
-  description: 'A user that can view and browse the map',
-})
+// await db.insert(roles).values({
+//   id: 'user',
+//   name: 'User',
+//   description: 'A user that can view and browse the map',
+// })
 
-await db.insert(roles).values({
-  id: 'admin',
-  name: 'Administrator',
-  description:
-    'An administrator that can modify the app configuration and behavior',
-})
+// await db.insert(roles).values({
+//   id: 'admin',
+//   name: 'Administrator',
+//   description:
+//     'An administrator that can modify the app configuration and behavior',
+// })
 
-await db.insert(permissions).values({
-  id: 'map.view',
-  name: 'View map',
-})
+// await db.insert(permissions).values({
+//   id: 'map.view',
+//   name: 'View map',
+// })
 
-await db.insert(usersToRoles).values({
-  userId: '1',
-  roleId: 'admin',
-})
+// await db.insert(permissions).values({
+//   id: 'map.update',
+//   name: 'Modify map settings',
+// })
 
-await db.insert(roleToPermissions).values({
-  roleId: 'admin',
-  permissionId: 'map.view',
-})
+// await db.insert(usersToRoles).values({
+//   userId: '1',
+//   roleId: 'admin',
+// })
+
+// await db.insert(usersToRoles).values({
+//   userId: '1',
+//   roleId: 'user',
+// })
+
+// await db.insert(roleToPermissions).values({
+//   roleId: 'user',
+//   permissionId: 'map.read',
+// })
+
+// await db.insert(roleToPermissions).values({
+//   roleId: 'admin',
+//   permissionId: 'map.update',
+// })
 
 await connection.close()

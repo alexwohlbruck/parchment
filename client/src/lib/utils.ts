981ceb26aa5745917fc331b44070a34717afc874
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-07-30 23:52:53 -0400

    Add breakpoint conditional rendering

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,7 +1,33 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { computed } from 'vue'
+import { useWindowSize } from '@vueuse/core'
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import resolveConfig from 'tailwindcss/resolveConfig'
+import tailwindConfig from '../../tailwind.config.js'
+
 // import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
 
+const tailwind = resolveConfig(tailwindConfig)
+
+export function getBreakpoints() {
+  return tailwind.theme.screens
+}
+
+export function useResponsive() {
+  const { width } = useWindowSize()
+  const breakpoints = getBreakpoints()
+
+  const isSmallScreen = computed(() => width.value < parseInt(breakpoints.md))
+  const isMediumScreen = computed(
+    () =>
+      width.value >= parseInt(breakpoints.md) &&
+      width.value < parseInt(breakpoints.lg),
+  )
+  const isLargeScreen = computed(() => width.value >= parseInt(breakpoints.lg))
+
+  return { isSmallScreen, isMediumScreen, isLargeScreen }
+}
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }

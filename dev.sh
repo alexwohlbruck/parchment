d252672b922e59e5851e073d53b81b3c6ad5d71e
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-11-13 23:47:38 -0500

    Merge dev and prod shell scripts, add options for auto db seed and migrate

--- dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DOCKERFILE=Dockerfile.dev
-export NODE_ENV=development
-docker-compose up --build web
--- prod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DOCKERFILE=Dockerfile.prod
-export NODE_ENV=production
-docker-compose up --build backend server-db

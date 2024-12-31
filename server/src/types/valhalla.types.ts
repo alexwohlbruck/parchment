80f30b2d10007b34f975ac1b793f9382b97fb305
Alex Wohlbruck <alexwohlbruck@gmail.com>
2024-12-31 15:06:10 -0500

    Add stops layer on map

--- server/src/types/valhalla.types.ts
+++ server/src/types/valhalla.types.ts
@@ -11,6 +11,29 @@ export type Directions = {
       time: number
     }
   }>
+  locations: Array<{
+    lat: number
+    lon: number
+    type?: string
+    original_index?: number
+  }>
+  language: string // TODO: We have a type for Locale
+  status: number
+  status_message: string
+  summary: {
+    cost: number
+    has_ferry: boolean
+    has_highway: boolean
+    has_time_restrictions: boolean
+    has_toll: boolean
+    length: number
+    max_lat: number
+    max_lon: number
+    min_lat: number
+    min_lon: number
+    time: number
+  }
+  units: string
 }
 
 export type ValhallaRouteRequest = {

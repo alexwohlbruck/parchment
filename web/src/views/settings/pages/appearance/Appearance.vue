99615fa93c889af1ab227bb5dedff709b0554d21
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-01-03 18:30:50 -0500

    Update appearance settings with new card component

--- web/src/views/settings/pages/appearance/Appearance.vue
+++ web/src/views/settings/pages/appearance/Appearance.vue
@@ -18,53 +18,47 @@ import {
   SelectValue,
 } from '@/components/ui/select'
 import ThemeControls from './ThemeControls.vue'
+import { SettingsCard, SettingsItem } from '@/components/settings'
 
 const mapTheme = ref('auto')
 </script>
 
 <template>
   <div class="flex flex-col gap-4 w-fit">
-    <div>
-      <H6>{{ $t('settings.appearance.appTheme.title') }}</H6>
+    <SettingsCard :title="$t('settings.appearance.appTheme.title')">
       <ThemeControls class="mt-3" />
-    </div>
+    </SettingsCard>
 
-    <form class="flex flex-col gap-4">
-      <FormField name="map-theme">
-        <FormItem>
-          <FormLabel>{{ $t('settings.appearance.mapTheme.title') }}</FormLabel>
-
-          <Select v-model="mapTheme">
-            <FormControl>
-              <SelectTrigger>
-                <SelectValue placeholder="Choose an option" />
-              </SelectTrigger>
-            </FormControl>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value="auto">
-                  {{ $t('settings.appearance.mapTheme.values.matchTime') }}
-                </SelectItem>
-                <SelectItem value="match">{{
-                  $t('settings.appearance.mapTheme.values.match')
-                }}</SelectItem>
-                <SelectItem value="light">{{
-                  $t('settings.appearance.mapTheme.values.day')
-                }}</SelectItem>
-                <SelectItem value="dark">{{
-                  $t('settings.appearance.mapTheme.values.night')
-                }}</SelectItem>
-                <SelectItem value="dawn">{{
-                  $t('settings.appearance.mapTheme.values.dawn')
-                }}</SelectItem>
-                <SelectItem value="dusk">{{
-                  $t('settings.appearance.mapTheme.values.dusk')
-                }}</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </FormItem>
-      </FormField>
-    </form>
+    <SettingsCard :title="$t('settings.appearance.mapTheme.title')">
+      <SettingsItem :title="$t('settings.appearance.mapTheme.title')">
+        <Select v-model="mapTheme">
+          <SelectTrigger class="w-fit">
+            <SelectValue placeholder="Choose an option" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="auto">
+                {{ $t('settings.appearance.mapTheme.values.matchTime') }}
+              </SelectItem>
+              <SelectItem value="match">{{
+                $t('settings.appearance.mapTheme.values.match')
+              }}</SelectItem>
+              <SelectItem value="light">{{
+                $t('settings.appearance.mapTheme.values.day')
+              }}</SelectItem>
+              <SelectItem value="dark">{{
+                $t('settings.appearance.mapTheme.values.night')
+              }}</SelectItem>
+              <SelectItem value="dawn">{{
+                $t('settings.appearance.mapTheme.values.dawn')
+              }}</SelectItem>
+              <SelectItem value="dusk">{{
+                $t('settings.appearance.mapTheme.values.dusk')
+              }}</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </SettingsItem>
+    </SettingsCard>
   </div>
 </template>

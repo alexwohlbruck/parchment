1c642913936d0045f47a29c53090126a12c23ad6
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-01-04 22:45:26 -0500

    Update focus styles

--- web/src/components/ui/checkbox/Checkbox.vue
+++ web/src/components/ui/checkbox/Checkbox.vue
@@ -19,7 +19,7 @@ const forwarded = useForwardPropsEmits(props, emits)
     v-bind="forwarded"
     :class="
       cn(
-        'peer size-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
+        'peer size-4 shrink-0 rounded-sm border border-primary ring-offset-background transition-shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-inset focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground',
         $attrs.class ?? '',
       )
     "

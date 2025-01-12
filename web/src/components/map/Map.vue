8ffcb242dbdb3334bcea649eadea2f73b80be232
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-01-12 16:06:32 -0500

    Implement "Directions to" and "Directions from" context menu buttons

--- web/src/components/map/Map.vue
+++ web/src/components/map/Map.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref, useTemplateRef } from 'vue'
+import { onMounted, onUnmounted, ref, useTemplateRef, computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { storeToRefs } from 'pinia'
 import { useMapStore } from '../../stores/map.store'
 import { useMapService } from '@/services/map.service'
 import { useAppService } from '@/services/app.service'
+import { useDirectionsService } from '@/services/directions.service'
 import { MapStrategy } from './map-providers/map.strategy'
 import { mapEventBus } from '@/lib/eventBus'
 import { LngLat } from '@/types/map.types'
+import { useDirectionsStore } from '@/stores/directions.store'
 
 import {
   DropdownMenu,
@@ -16,16 +20,21 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 
+const router = useRouter()
+const appService = useAppService()
 const mapService = useMapService()
 const mapStore = useMapStore()
-const appService = useAppService()
+const directionsService = useDirectionsService()
+const directionsStore = useDirectionsStore()
+
+const { waypoints } = storeToRefs(directionsStore)
 
 const mapContainer = useTemplateRef<HTMLElement>('mapContainer')
 let mapStrategy: MapStrategy
 
 const showContextMenu = ref(false)
 const contextMenuPosition = ref({ x: 0, y: 0 })
-const clickedLngLat = ref<LngLat>()
+const clickedLngLat = ref<LngLat | null>(null)
 
 onMounted(() => {
   if (!mapContainer.value) {
@@ -44,10 +53,42 @@ onUnmounted(() => {
   mapService.destroy()
 })
 
-const copyCoordinates = (lngLat: LngLat) => {
+function copyCoordinates(lngLat: LngLat) {
   const coordString = `${lngLat.lat}, ${lngLat.lng}`
   navigator.clipboard.writeText(coordString)
   appService.toast.info('Coordinates copied to clipboard')
+}
+
+const useMultistopDirections = computed(() => {
+  const filledWaypointsCount = waypoints.value.reduce(
+    (count, waypoint) => (waypoint.lngLat ? count + 1 : count),
+    0,
+  )
+  console.log(filledWaypointsCount, waypoints.value.length)
+  return waypoints.value.length > 2 || filledWaypointsCount >= 2
+})
+
+function directionsTo() {
+  router.push('/directions')
+  directionsService.directionsTo({
+    lngLat: clickedLngLat.value,
+  })
+}
+
+function directionsFrom() {
+  router.push('/directions')
+  directionsService.directionsFrom({
+    lngLat: clickedLngLat.value,
+  })
+}
+
+function fillWaypoint() {
+  router.push('/directions')
+  setTimeout(() => {
+    directionsService.fillWaypoint({
+      lngLat: clickedLngLat.value,
+    })
+  }, 0)
 }
 </script>
 
@@ -75,8 +116,21 @@ const copyCoordinates = (lngLat: LngLat) => {
         >
           {{ clickedLngLat.lat.toFixed(5) }}, {{ clickedLngLat.lng.toFixed(5) }}
         </DropdownMenuItem>
-        <DropdownMenuItem> Directions to here </DropdownMenuItem>
-        <DropdownMenuItem> Directions from here </DropdownMenuItem>
+        <DropdownMenuItem
+          v-if="!useMultistopDirections"
+          @click="directionsTo()"
+        >
+          Directions to here
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          v-if="!useMultistopDirections"
+          @click="directionsFrom()"
+        >
+          Directions from here
+        </DropdownMenuItem>
+        <DropdownMenuItem v-if="useMultistopDirections" @click="fillWaypoint()">
+          Add stop
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   </div>
--- web/src/services/directions.service.ts
+++ web/src/services/directions.service.ts
@@ -1,18 +1,94 @@
+import { watch } from 'vue'
+import { storeToRefs } from 'pinia'
 import { api } from '@/lib/api'
 import { createSharedComposable } from '@vueuse/core'
-import type { Location, ValhallaRouteRequest } from '@/types/directions.types'
 import { useDirectionsStore } from '@/stores/directions.store'
+import { Waypoint } from '@/types/map.types'
 
-const directionsStore = useDirectionsStore()
+const MIN_LOCATIONS = 2
 
 function directionsService() {
-  async function getDirections(payload: ValhallaRouteRequest) {
-    const { data: directions } = await api.post('/directions', payload)
-    return directions
+  const directionsStore = useDirectionsStore()
+  const { waypoints, selectedMode } = storeToRefs(directionsStore)
+
+  async function getDirections() {
+    const filteredWaypoints = waypoints.value.filter(wp => wp.lngLat != null)
+
+    if (filteredWaypoints.length < MIN_LOCATIONS) {
+      return
+    }
+
+    const { data: directions } = await api.post('/directions', {
+      locations: filteredWaypoints.map(location => ({
+        type: 'coordinates',
+        value: [location.lngLat!.lat, location.lngLat!.lng],
+      })),
+      costing: selectedMode.value,
+    })
+
+    directionsStore.setDirections(directions)
   }
+
+  function createBlankWaypoint() {
+    return {
+      lngLat: null,
+    }
+  }
+
+  // Fill a new waypoint into an available slot
+  function fillWaypoint(waypoint: Waypoint) {
+    const emptyIndex = waypoints.value.findIndex(wp => wp.lngLat === null)
+    if (emptyIndex !== -1) {
+      directionsStore.setWaypoint(emptyIndex, waypoint)
+    } else {
+      directionsStore.setWaypoint(waypoints.value.length, waypoint)
+    }
+  }
+
+  function setWaypoint(index: number, waypoint: Waypoint) {
+    directionsStore.setWaypoint(index, waypoint)
+  }
+
+  function setWaypoints(waypoints: Waypoint[]) {
+    directionsStore.setWaypoints(waypoints)
+  }
+
+  function removeWaypoint(index: number) {
+    if (index >= MIN_LOCATIONS - 1) {
+      directionsStore.removeWaypoint(index)
+    } else {
+      directionsStore.setWaypoint(index, {
+        ...waypoints.value[index],
+        lngLat: null,
+      })
+    }
+  }
+
+  async function directionsFrom(waypoint: Waypoint) {
+    directionsStore.setWaypoint(0, waypoint)
+  }
+
+  async function directionsTo(waypoint: Waypoint) {
+    directionsStore.setWaypoint(1, waypoint)
+  }
+
+  watch(selectedMode, () => {
+    getDirections()
+  })
+
+  watch(waypoints, () => {
+    getDirections()
+  })
 
   return {
     getDirections,
+    createBlankWaypoint,
+    fillWaypoint,
+    setWaypoint,
+    setWaypoints,
+    removeWaypoint,
+    directionsFrom,
+    directionsTo,
   }
 }
 

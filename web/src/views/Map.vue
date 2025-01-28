79eb2bd28e4c7b99344213c1dc863d2af3e985f2
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-01-27 20:29:39 -0500

    Move map controls to map view

--- web/src/views/Map.vue
+++ web/src/views/Map.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
+import { storeToRefs } from 'pinia'
+import { useMapStore } from '@/stores/map.store'
 import Map from '@/components/map/Map.vue'
+import { TransitionExpand } from '@morev/vue-transitions'
+import StreetView from '@/components/map/StreetView.vue'
+
 const route = useRoute()
+const mapStore = useMapStore()
+
+const { streetView } = storeToRefs(mapStore)
 </script>
 
 <template>
@@ -14,4 +22,18 @@ const route = useRoute()
   </div>
 
   <Map class="!absolute w-full h-full top-0 left-0"></Map>
+
+  <div
+    class="w-full absolute bottom-[7.5rem] md:bottom-0 right-0 z-50 p-2 flex flex-col gap-2 items-end pointer-events-none"
+  >
+    <LayerControls />
+
+    <TransitionExpand>
+      <StreetView
+        v-if="streetView"
+        :image="streetView"
+        class="pointer-events-auto rounded-lg shadow-md w-full md:w-[40vw] aspect-video"
+      />
+    </TransitionExpand>
+  </div>
 </template>

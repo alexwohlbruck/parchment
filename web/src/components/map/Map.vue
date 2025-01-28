3f6781fb7dfc9f36103254edb72f277d8426886b
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-01-27 20:25:04 -0500

    Move layer controls to dedicated component

--- web/src/components/map/Map.vue
+++ web/src/components/map/Map.vue
@@ -4,20 +4,9 @@ import { storeToRefs } from 'pinia'
 import { useMapStore } from '../../stores/map.store'
 import { useMapService } from '@/services/map.service'
 import { MapStrategy } from './map-providers/map.strategy'
-import { LayerType } from '@/types/map.types'
 import { TransitionExpand } from '@morev/vue-transitions'
-import { Card } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
-import { Toggle } from '@/components/ui/toggle'
+
 import StreetView from '@/components/map/StreetView.vue'
-import { Layers3Icon, PersonStandingIcon } from 'lucide-vue-next'
-import LayersSelector from '@/components/navigation/LayersSelector.vue'
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from '@/components/ui/hover-card'
-import { layers } from '@/components/map/layers/layers'
 import ContextMenu from '@/components/map/ContextMenu.vue'
 
 const mapService = useMapService()
@@ -38,14 +27,6 @@ onMounted(() => {
 onUnmounted(() => {
   mapService.destroy()
 })
-
-function toggleStreetViewLayers() {
-  layers.forEach(layer => {
-    if (layer.type === LayerType.STREET_VIEW) {
-      mapService.toggleLayerVisibility(layer.configuration.id)
-    }
-  })
-}
 </script>
 
 <template>
@@ -53,34 +34,7 @@ function toggleStreetViewLayers() {
     <div
       class="w-full absolute bottom-[7.5rem] md:bottom-0 right-0 z-50 p-2 flex flex-col gap-2 items-end pointer-events-none"
     >
-      <div class="flex flex-col gap-2 pointer-events-auto">
-        <Card class="border-none">
-          <Toggle
-            variant="outline"
-            size="icon"
-            class="size-10"
-            @click="toggleStreetViewLayers()"
-          >
-            <PersonStandingIcon class="size-6" />
-          </Toggle>
-        </Card>
-
-        <HoverCard :openDelay="0" :closeDelay="0">
-          <HoverCardTrigger as-child>
-            <Button variant="outline" size="icon" class="size-10 shadow-md">
-              <Layers3Icon class="size-5" />
-            </Button>
-          </HoverCardTrigger>
-
-          <HoverCardContent
-            side="left"
-            align="end"
-            class="w-fit fit-content max-w-[calc(100vw-3.75rem)] md:max-w-[50vw]"
-          >
-            <LayersSelector />
-          </HoverCardContent>
-        </HoverCard>
-      </div>
+      <LayerControls />
 
       <TransitionExpand>
         <StreetView

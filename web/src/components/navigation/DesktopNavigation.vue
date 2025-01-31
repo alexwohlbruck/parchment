020ff14e99b61df4bc878910987936f4c4cbcd69
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-01-31 16:30:30 -0500

    Add transitions to ui layout

--- web/src/components/navigation/DesktopNavigation.vue
+++ web/src/components/navigation/DesktopNavigation.vue
@@ -111,81 +111,87 @@ const items = computed(() => {
 </script>
 
 <template>
-  <div
-    @mouseover="mini = false"
-    @mouseleave="mini = true"
-    :class="
-      cn(
-        'bg-background max-h-full overflow-y-auto m-2 py-2 shadow-md flex flex-col gap-2 rounded-md',
-        $attrs.class ?? '',
-      )
-    "
-  >
-    <h2 class="px-[.95rem] text-lg font-bold">
-      <span>Pa</span>
-      <transition-expand axis="x" :duration="50" easing="ease-out">
-        <span v-if="!mini" class="text-nowrap absolute"> rchment </span>
-      </transition-expand>
-    </h2>
+  <div class="flex flex-col justify-center">
+    <div
+      @mouseover="mini = false"
+      @mouseleave="mini = true"
+      :class="
+        cn(
+          'bg-background max-h-full overflow-y-auto m-2 py-2 shadow-md flex flex-col gap-2 rounded-md',
+          $attrs.class ?? '',
+        )
+      "
+    >
+      <h2 class="px-[.95rem] text-lg font-bold">
+        <span>Pa</span>
+        <transition-expand axis="x" :duration="50" easing="ease-out">
+          <span v-if="!mini" class="text-nowrap absolute"> rchment </span>
+        </transition-expand>
+      </h2>
 
-    <div v-for="(item, i) in items" :key="i">
-      <Separator v-if="item.separator" />
-      <div v-else>
-        <div class="flex flex-col px-1">
-          <template v-for="subitem in item.items">
-            <!-- v-if="subitem.to && (subitem.condition ?? true)" -->
-            <Button
-              v-if="subitem.to"
-              variant="ghost"
-              class="w-full flex px-3 justify-center gap-3 hover:bg-primary/5 hover:text-primary"
-              as-child
-              :to="subitem.to"
-              :class="
-                router.currentRoute.value.path === subitem.to
-                  ? 'bg-primary/5 text-primary'
-                  : ''
-              "
-            >
-              <router-link :to="subitem.to">
-                <component :is="subitem.icon" class="size-5" />
+      <div v-for="(item, i) in items" :key="i">
+        <Separator v-if="item.separator" />
+        <div v-else>
+          <div class="flex flex-col px-1">
+            <template v-for="subitem in item.items">
+              <!-- v-if="subitem.to && (subitem.condition ?? true)" -->
+              <Button
+                v-if="subitem.to"
+                variant="ghost"
+                class="w-full flex px-3 justify-center gap-3 hover:bg-primary/5 hover:text-primary"
+                as-child
+                :to="subitem.to"
+                :class="
+                  router.currentRoute.value.path === subitem.to
+                    ? 'bg-primary/5 text-primary'
+                    : ''
+                "
+              >
+                <router-link :to="subitem.to">
+                  <component :is="subitem.icon" class="size-5" />
 
-                <transition-expand axis="x" :duration="50" easing="ease-out">
-                  <div v-if="!mini" class="flex flex-1 gap-1 text-nowrap">
-                    <div class="flex-1">
-                      {{ subitem.label }}
+                  <transition-expand axis="x" :duration="50" easing="ease-out">
+                    <div v-if="!mini" class="flex flex-1 gap-1 text-nowrap">
+                      <div class="flex-1">
+                        {{ subitem.label }}
+                      </div>
+
+                      <!-- <Kbd v-if="subitem.hotkey" :hotkey="subitem.hotkey"></Kbd> -->
                     </div>
-
-                    <!-- <Kbd v-if="subitem.hotkey" :hotkey="subitem.hotkey"></Kbd> -->
-                  </div>
-                </transition-expand>
-              </router-link>
-            </Button>
-          </template>
+                  </transition-expand>
+                </router-link>
+              </Button>
+            </template>
+          </div>
         </div>
       </div>
+
+      <router-link
+        to="/settings/account"
+        v-if="me"
+        class="px-2.5 flex items-center gap-2"
+      >
+        <Avatar size="xs">
+          <AvatarImage
+            v-if="me.picture"
+            :src="me.picture"
+            alt="@alexwohlbruck"
+          />
+          <AvatarFallback v-else>
+            {{ me.firstName?.charAt(0) }} {{ me.lastName?.charAt(0) }}
+          </AvatarFallback>
+        </Avatar>
+
+        <transition-expand axis="x" :duration="50" easing="ease-out">
+          <div class="flex flex-col text-nowrap" v-if="!mini">
+            <span class="text-sm font-semibold leading-4">
+              {{ me.firstName }} {{ me.lastName }}
+            </span>
+            <span class="text-xs text-gray-500 leading-4">{{ me.email }}</span>
+          </div>
+        </transition-expand>
+      </router-link>
     </div>
-
-    <router-link
-      to="/settings/account"
-      v-if="me"
-      class="px-2.5 flex items-center gap-2"
-    >
-      <Avatar size="xs">
-        <AvatarImage v-if="me.picture" :src="me.picture" alt="@alexwohlbruck" />
-        <AvatarFallback v-else>
-          {{ me.firstName?.charAt(0) }} {{ me.lastName?.charAt(0) }}
-        </AvatarFallback>
-      </Avatar>
-
-      <transition-expand axis="x" :duration="50" easing="ease-out">
-        <div class="flex flex-col text-nowrap" v-if="!mini">
-          <span class="text-sm font-semibold leading-4">
-            {{ me.firstName }} {{ me.lastName }}
-          </span>
-          <span class="text-xs text-gray-500 leading-4">{{ me.email }}</span>
-        </div>
-      </transition-expand>
-    </router-link>
   </div>
 </template>
 @/stores/auth

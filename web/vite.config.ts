5c40c1f20a64a05518d7786203188ff939e8fb3f
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-02-09 02:33:54 -0500

    Begin Tauri mobile port

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
+const host = process.env.TAURI_DEV_HOST
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -15,5 +17,14 @@ export default defineConfig({
   },
   server: {
     port: parseInt(process.env.VITE_PORT || '5173'),
+    // host: host || false,
+    // strictPort: true,
   },
+  // envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  // build: {
+  //   target:
+  //     process.env.TAURI_ENV_PLATFORM === 'windows' ? 'chrome105' : 'safari13',
+  //   minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+  //   sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  // },
 })

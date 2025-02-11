95059b0c47bfc9f3df2fb99b191172c032ec8934
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-02-10 23:24:46 -0500

    Enable tauri CORS

--- server/src/config/cors.config.ts
+++ server/src/config/cors.config.ts
@@ -3,7 +3,12 @@ import { clientHostname } from './origins.config'
 
 const allowedMethods: HTTPMethod[] = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 export default {
-  origin: [clientHostname, 'tauri://localhost'],
+  origin: [
+    clientHostname,
+    'tauri://localhost',
+    'http://tauri.localhost',
+    'http://10.0.2.2:5173',
+  ],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Set-Cookie'],
   exposedHeaders: '*',

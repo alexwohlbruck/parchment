1d793188835bbf978f2586ffb87e74a90922c027
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-02-11 20:10:07 -0500

    Enable additional headers on CORS config

--- server/src/config/cors.config.ts
+++ server/src/config/cors.config.ts
@@ -7,11 +7,16 @@ export default {
     clientHostname,
     'tauri://localhost',
     'http://tauri.localhost',
-    'http://tauri.localhost/',
     'http://10.0.2.2:5173',
   ],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Set-Cookie'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'Set-Cookie',
+    'X-Requested-With',
+    'Accept',
+  ],
   exposedHeaders: '*',
   methods: allowedMethods,
 }

e3b549b4500fdf6820c1ca6ea9898b003b9cb9ee
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-05-02 00:32:03 -0400

    Add drizzle-orm dependency

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,14 +1,12 @@
 import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 import * as dotenv from 'dotenv'
-import * as schema from './schema/library.schema'
 
 dotenv.config({
   path: '.env.local',
 })
 
 export const dbUrl = process.env.DATABASE_URL as string
-export const dbUrlLocal = process.env.DATABASE_URL_LOCAL as string
 
 export const connection = postgres(dbUrl)
-export const db = drizzle(connection, { schema })
+export const db = drizzle(connection)

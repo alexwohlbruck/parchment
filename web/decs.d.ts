03bfb5b00f6189f10c00c9601213111778b71c20
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-06-02 14:39:10 -0400

    Fixes for docker production build

--- web/decs.d.ts
+++ web/decs.d.ts
@@ -1,4 +1,9 @@
-declare module '*.vue'
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
 declare module '@morev/vue-transitions'
 declare module 'path'
 

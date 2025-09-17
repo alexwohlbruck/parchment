af5ddd55cfbac44a98bf91d4278438cb6a6945e1
Alex Wohlbruck <alexwohlbruck@gmail.com>
2025-09-17 00:48:59 -0400

    Fix spacing & db connection

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,7 +1,12 @@
 import postgres from 'postgres'
 import { drizzle } from 'drizzle-orm/postgres-js'
 
-export const dbUrl = process.env.DATABASE_URL as string
+export const dbUser = process.env.POSTGRES_USER as string
+export const dbPassword = process.env.POSTGRES_PASSWORD as string
+export const dbHost = process.env.POSTGRES_HOST as string
+export const dbName = process.env.POSTGRES_DB as string
+
+export const dbUrl = `postgresql://${dbUser}:${dbPassword}@${dbHost}/${dbName}?sslmode=require`
 
 export const connection = postgres(dbUrl)
 export const db = drizzle(connection)

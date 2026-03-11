b4190d663f2b654008e1a9ff89d3f2a6a5536fe5
Alex Wohlbruck <alexwohlbruck@gmail.com>
2026-03-11 07:49:16 -0400

    Move docs to standalone repo

--- docs/lib/source.ts
+++
@@ -1,7 +0,0 @@
-import { docs } from 'collections/index'
-import { loader } from 'fumadocs-core/source'
-
-export const source = loader({
-  baseUrl: '/docs',
-  source: docs.toFumadocsSource(),
-})

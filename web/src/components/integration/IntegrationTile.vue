74d9e34e7c5d873bb99ecc601137bd537e425414
Alex Wohlbruck <alexwohlbruck@gmail.com>
2026-09-09 11:34:56 -0400

    Link integration setup warning to identity settings

--- web/src/components/integration/IntegrationTile.vue
+++ web/src/components/integration/IntegrationTile.vue
@@ -12,6 +12,7 @@ import {
   IntegrationScope,
 } from '@/types/integrations.types'
 import { computed, h } from 'vue'
+import { useRouter } from 'vue-router'
 import { storeToRefs } from 'pinia'
 import { Button } from '@/components/ui/button'
 import { useIntegrationsStore } from '@/stores/integrations.store'
@@ -30,6 +31,7 @@ import {
 } from '@/components/ui/tooltip'
 
 const { t } = useI18n()
+const router = useRouter()
 const integrationsStore = useIntegrationsStore()
 const identityStore = useIdentityStore()
 const { isSetupComplete } = storeToRefs(identityStore)
@@ -249,7 +251,13 @@ async function handleClick() {
   const integration = props.integration
 
   if (requiresSetup.value) {
-    toast.warning(t('settings.integrations.scheme.setupRequired'))
+    toast.warning(t('settings.integrations.scheme.setupRequired'), {
+      action: {
+        label: t('settings.integrations.scheme.setupRequiredAction'),
+        onClick: () =>
+          router.push({ path: '/settings/account', hash: '#identity' }),
+      },
+    })
     return
   }
 
